0c5ce8e8aa772aea476f9da60280550ba943b98f
xd <dongxiechn@gmail.com>
2020-07-28 17:28:50 +0800

    进一步优化gradle配置 增加config server模块

---
+++ config-server/src/main/java/com/xd/springclouddemo/controller/ConfigController.java
@@ -0,0 +1,10 @@
+package com.xd.springclouddemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 配置控制器
+ */
+@RestController
+public class ConfigController {
+}
--- web/src/test/java/com/xd/springclouddemo/WebApplicationTests.java
+++ web/src/test/java/com/xd/springclouddemo/WebApplicationTests.java
@@ -3,11 +3,20 @@ package com.xd.springclouddemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+//@SpringBootTest
 class WebApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void instantTest() {
+        Instant instant = Instant.parse("2020-07-28T07:03:00.00Z");
+        System.out.println(ChronoUnit.MINUTES.between(instant, Instant.now()));
     }
 
 }

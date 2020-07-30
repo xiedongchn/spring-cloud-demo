9716cb9c88aa2dc6de35c44d5ac2dff097f7830d
xd <dongxiechn@gmail.com>
2020-07-30 17:39:44 +0800

    优化gradle配置

--- user/user-service/src/test/java/com/xd/springclouddemo/WebApplicationTests.java
+++ user/user-service/src/test/java/com/xd/springclouddemo/WebApplicationTests.java
@@ -1,11 +1,15 @@
 package com.xd.springclouddemo;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
-//@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class WebApplicationTests {
 
     @Test

a6b161ed7f98886ebc2140d47eb9be097f3bcd08
xiedongchn <xiedongchn@gmail.com>
2020-08-04 23:28:43 +0800

    change config

--- user/user-service/src/main/java/com/xd/springclouddemo/WebApplication.java
+++ user/user-service/src/main/java/com/xd/springclouddemo/WebApplication.java
@@ -2,11 +2,10 @@ package com.xd.springclouddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableConfigServer
-@SpringBootApplication(scanBasePackages = "com.xd.springclouddemo.controller")
+//@ComponentScan(basePackages = "com.xd.springclouddemo")
+@SpringBootApplication
 public class WebApplication {
 
     public static void main(String[] args) {
